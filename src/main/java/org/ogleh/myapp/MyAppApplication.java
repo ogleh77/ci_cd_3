34478ddd0b32005474b8f8eb0a710eeeeecb98f8
Mohamed Ogleh <mohamedogleh0@gmail.com>
2024-09-06 19:31:28 +0300

    added greet with name and changed hello to ASc.

--- src/main/java/org/ogleh/myapp/MyAppApplication.java
+++ src/main/java/org/ogleh/myapp/MyAppApplication.java
@@ -2,12 +2,25 @@ package org.ogleh.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MyAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyAppApplication.class, args);
-	}
+    @GetMapping("/")
+    public String greet() {
+        return "Asc World!";
+    }
 
+    @GetMapping("/{name}")
+    public String greetWithName(@PathVariable("name") String name) {
+        return "Asc " + name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAppApplication.class, args);
+    }
 }
